602879a73d265877683377299f1560006c356f45
Ke Wen <kw2501@meta.com>
2024-06-15 16:18:55 -0700

    Update dtype example with print

--- dtype.py
+++ dtype.py
@@ -17,10 +17,22 @@ class M(torch.nn.Module):
 x = torch.LongTensor([[1, 2, 4, 5], [4, 3, 2, 9]])
 
 mod = M()
+mod.to(torch.bfloat16)
 
 exported_program: torch.export.ExportedProgram = torch.export.export(
     mod, args=(x,)
 )
 
 exported_program.graph.print_tabular()
+
+exported_program.graph_module.print_readable()
+
+for node in exported_program.graph_module.graph.nodes:
+    try:
+        val = node.meta["val"]
+        dtype = val.dtype
+    except:
+        val = None
+        dtype = None
+    print(node.name, val, dtype)
 
